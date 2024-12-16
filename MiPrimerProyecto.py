6079f66ec97d31782c8207de03c8219f472066b5
Ramón <ramonortizcab35@gmail.com>
2024-12-16 14:31:19 +0100

    inicial

---
+++ MiPrimerProyecto.py
@@ -0,0 +1,22 @@
+edad=int(input("Dime tu edad: "))
+if edad>=18:
+    print("Eres mayor de edad")
+print("Programa Terminado")
+
+#funciones
+import math
+
+num=5
+abs(num)
+
+barra= 7/2
+
+barraDoble=7//2
+
+modulo=7%2
+
+pow(3**2)
+
+divmod(7,2)
+
+math.sqrt(9)
